fbf934de23007cbb18c5b489220262d2ec5bd336
Alumasa <alumasa.v@gmail.com>
2019-12-28 22:49:55 +0300

    Shopping calc v2

--- shopping_calculator.py
+++ shopping_calculator.py
@@ -1,20 +1,41 @@
-banana_cost = 5
-money = 20
-input_count = input("How many bananas do you want?:")
-count = int(input_count)
-total_cost = banana_cost * count
 
-print("You have bought " + str(input_count) + " bananas.")
-print("The total price is " + str(total_cost) + " Kenya Shillings")
+#foods = [rice, beans, maize, eggs, bread]
+#fruits = [apples, bananas, grapes, mangoes, oranges]
+banana_cost = 10
+apple_cost = 25
+mango_cost = 10
+#input_count = 0
+
+input_fruit = input("""Choose between: Banana, Apple and Mango.
+Which fruit would you like to buy?:""")
+
+input_count = input("How many " + input_fruit.lower() + "s do you want?:")
+count = int(input_count)
+
+if input_fruit.lower() == "banana" and input_fruit.isalpha():
+    total_cost = banana_cost * count
+elif input_fruit.lower() == "apple" and input_fruit.isalpha():
+    total_cost = apple_cost * count
+elif input_fruit.lower() == "mango" and input_fruit.isalpha():
+    total_cost = mango_cost * count
+else:
+    print("empty")
+
+
+input_money = input("How much money do you have?:")
+money = int(input_money)
+
+#print("You have bought " + str(input_count) + " " + input_fruit.lower() + "s.")
+print("The total price is " + str(total_cost) + " " + " Kenya Shillings")
 
 if money > int(total_cost):
-     print("You have bought " + str(input_count) + " bananas.")
+     print("You have bought " + str(input_count) + " " + input_fruit.lower() + "s.")
      print("You have " + str(money - int(total_cost)) + " Kenya Shillings left.")
 
 elif money == int(total_cost):
-     print("You have bought " + str(input_count) + " bananas.")
+     print("You have bought " + str(input_count) + " " + input_fruit.lower() + "s.")
      print("Your wallet is empty.")
 
 else:
-     print("You dont have enough money.")
-     print("You cannot buy that many bananas.")
+     print("You don\'t have enough money.")
+     print("You cannot buy that many " + input_fruit.lower() + "s.")
