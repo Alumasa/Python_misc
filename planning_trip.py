39153c7453a38900c327daa02940e64b95ebb832
Alumasa <alumasa.v@gmail.com>
2019-12-30 17:06:03 +0300

    second changes to planning trip

--- planning_trip.py
+++ planning_trip.py
@@ -1,10 +1,48 @@
 #this calculates cost of a holiday in kenya using functions
-#this functions calculates the cost of hotel rooms with number of nights
-def hotel_cost(nights):
+#this function calculates the cost of hotel rooms with number of nights
+
+vanessa = {"name": "vanessa",
+           "telephone": "07123456",
+           "home": "Kakamega"
+           }
+
+alumasa = {"name": "alumasa",
+           "telephone": "07986543",
+           "home": "Nairobi"
+           }
+chantal = {"name": "chantal",
+           "telephone": "07654320",
+           "home": "Mombasa"}
+
+username = input("Please enter your username: ")
+
+#customers1 = ["vanessa", "alumasa", "chantal"]
+def customer_details(customers):
+        if vanessa["name"] == username:
+            print(vanessa)
+        elif alumasa["name"] == username:
+            print(alumasa)
+        elif chantal["name"] == username:
+            print(chantal)
+        else:
+            print("Username not found in database.")
+
+customers = [vanessa, alumasa, chantal]
+customer_details(customers)
+
+confirm = input("Please confirm your details before planning your trip. y/n: ")
+
+
+
+
+def hotel_cost():
+    hotel_nights = input("How many nights will you spend?: ")
+    nights = int(hotel_nights)
     return 2000 * nights
 
 #this function calculates transport costs
-def plane_ride_cost(city):
+def plane_ride_cost():
+    city = input("Where would you like to go?: ")
     if city == "Nairobi":
         return 5500
     elif city == "Mombasa":
@@ -15,7 +53,9 @@ def plane_ride_cost(city):
         return 5000
 
 #this function calculates local transport car hire
-def rental_car_cost(days):
+def rental_car_cost():
+    car_days = input("How many days will you hire a car?: ")
+    days = int(car_days)
     total = 5000 * days
     if days >= 7:
         total -= 1000
@@ -24,8 +64,21 @@ def rental_car_cost(days):
     return total
 
 #this function calls other functions and addds them to find the total cost of the trip.
-def trip_cost(city, days, spending_money):
-    return rental_car_cost(days) + hotel_cost(days - 1) + plane_ride_cost(city) + spending_money
+def trip_cost():
+    return rental_car_cost() + hotel_cost() + plane_ride_cost() + spending_money
 
-#output of thr trip cost function
-print(trip_cost("Mombasa", 5, 5000))
+
+
+if confirm == "y":
+    spending_money = int(input("How much is your extra money?: "))
+    #hotel_cost()
+    #plane_ride_cost()
+    #rental_car_cost()
+    #trip_cost()
+    print(trip_cost())
+else:
+    print("Please change your details before planning.")
+
+
+
+#output of the trip cost function
